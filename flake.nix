cf41b7a0cc63a2851780ba075b05791e536cde5d
Lucas ROUAUD <lucas.rouaud@gmail.com>
2025-08-20 21:47:42 +0200

    For now, enable a 'dev-shell' for testing.

--- flake.nix
+++ flake.nix
@@ -1,35 +1,52 @@
 {
+  description = "Enable the `strange` Nix environment.";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # All latest packages.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     nvf.url = "github:notashelf/nvf";
   };
 
-  outputs = {nixpkgs, ...} @ inputs: {
-    packages.x86_64-linux = {
-      # Set the default package to the wrapped instance of Neovim.
-      # This will allow running your Neovim configuration with
-      # `nix run` and in addition, sharing your configuration with
-      # other users in case your repository is public.
-      default =
-        (inputs.nvf.lib.neovimConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          modules = [
-            {
-              config.vim = {
-                # Enable custom theming options
-                theme.enable = true;
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , nvf
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
 
-                # Enable Treesitter
-                treesitter.enable = true;
+      configModule = {
+        config.vim = {
+          theme = {
+	    enable = true;
+	    name = "nord";
+	  };
 
-                # Other options will go here. Refer to the config
-                # reference in Appendix B of the nvf manual.
-                # ...
-              };
-            }
-          ];
-        })
-        .neovim;
-    };
-  };
+	  treesitter.enable = true;
+
+          filetree = {
+	    nvimTree.setupOpts.view.number = true;
+	    nvimTree.setupOpts.view.relativenumber = true;
+	  };
+        };
+      };
+
+      nvim = nvf.lib.neovimConfiguration {
+        inherit pkgs;
+        modules = [configModule];
+      };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        name = "nvf";
+
+        buildInputs = [
+	  nvim.neovim
+        ];
+      };
+    }
+    );
 }
