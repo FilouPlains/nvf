9a6043d11b29e39bcd2bd9e1f6b83d7170628016
Lucas ROUAUD <lucas.rouaud@gmail.com>
2025-09-18 11:11:53 +0200

    Adding new keymaps.

--- core/keymap.nix
+++ core/keymap.nix
@@ -1,10 +1,22 @@
+{ }:
+
+{
   vim.keymaps = [
+    # Undo.
     {
-      mode = ["n", "v"];
-      key = "<C-u>";
+      mode = ["n" "v" "i"];
+      key = "<C-z>";
       silent = true;
       action = "<cmd>u<CR>";
     }
+    # Redo.
+    {
+      mode = ["n" "v" "i"];
+      key = "<C-y>";
+      silent = true;
+      action = "<C-r>";
+    }
+    # Save.
     {
       mode = ["n"];
       key = "<C-s>";
@@ -23,5 +35,45 @@
       silent = true;
       action = "<Esc><cmd>w<CR>i<Right>";
     }
+    # Move lines down.
+    {
+      mode = ["n"];
+      key = "<M-Down>";
+      silent = true;
+      action = "<cmd>m +1<CR>";
+    }
+    {
+      mode = ["n"];
+      key = "<M-j>";
+      silent = true;
+      action = "<cmd>m +1<CR>";
+    }
+    # Move lines up.
+    {
+      mode = ["n"];
+      key = "<M-Up>";
+      silent = true;
+      action = "<cmd>m -2<CR>";
+    }
+    {
+      mode = ["n"];
+      key = "<M-k>";
+      silent = true;
+      action = "<cmd>m -2<CR>";
+    }
+    # Copy.
+    {
+      mode = ["v"];
+      key = "Y";
+      silent = true;
+      action = "\"+y";
+    }
+    # Retab.
+    {
+      mode = ["n"];
+      key = "<Leader-t>";
+      silent = true;
+      action = "<cmd>retab<CR>";
+    }
   ];
-
+}
