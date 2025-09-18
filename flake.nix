3b7d4ac08142b1e2c05078b6fd1c76960f9260a4
Lucas ROUAUD <lucas.rouaud@gmail.com>
2025-09-18 10:28:14 +0200

    Changing from dev shell to run command.

--- flake.nix
+++ flake.nix
@@ -40,13 +40,8 @@
       };
     in
     {
-      devShells.default = pkgs.mkShell {
-        name = "nvf";
-
-        buildInputs = [
-	  nvim.neovim
-        ];
-      };
+      # nix run .#nvim
+      packages.nvim = nvim.neovim;
     }
     );
 }
