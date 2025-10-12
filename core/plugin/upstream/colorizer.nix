427b8370390b1d22ac2e125d7e1d1aedcc7fdbc3
Lucas ROUAUD <lucas.rouaud@gmail.com>
2025-10-12 15:47:33 +0200

    Setup colorizer for all files.

--- core/plugin/upstream/colorizer.nix
+++ core/plugin/upstream/colorizer.nix
@@ -1,5 +1,22 @@
 _: {
-  ui.colorize = {
+  vim.ui.colorizer = {
     enable = true;
+
+    setupOpts = {
+      user_default_options = {
+        css = true;
+        mode = "background";
+
+        AARRGGBB = true;
+        RGB = true;
+        RRGGBB = true;
+        RRGGBBAA = true;
+        names = true;
+        css_fn = true;
+        tailwind = true;
+      };
+
+      filetypes."*" = _;
+    };
   };
 }
