5b56d34b79cd7e768f94324e771cec52486f6fa3
Lucas ROUAUD <lucas.rouaud@gmail.com>
2025-10-19 18:50:16 +0200

    Add notification plugin!

---
+++ core/plugin/upstream/notify.nix
@@ -0,0 +1,20 @@
+_: {
+  vim.notify.nvim-notify = {
+    enable = true;
+
+    setupOpts = {
+      position = "top_right";
+
+      icons = {
+        DEBUG = "󰮔 ";
+        ERROR = " ";
+        INFO = " ";
+        TRACE = "󰛿 ";
+        WARN = " ";
+      };
+
+      render = "default";
+      stages = "slide";
+    };
+  };
+}
