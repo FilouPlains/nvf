93b16a668246deb509e9a3a8c2246fe14d1197b9
Lucas ROUAUD <lucas.rouaud@gmail.com>
2025-12-15 01:26:45 +0100

    Updating again a keymap… Ctrk+arrow is for word moving, alt+arrow for moving text bloc, alt+ctrl+arrow moving window.

--- core/keymap.nix
+++ core/keymap.nix
@@ -244,8 +244,8 @@ in {
             mode = ["n"];
             key =
               if isArrow
-              then "<M-${name}>"
-              else "<M-${value}>";
+              then "<C-M-${name}>"
+              else "<C-M-${value}>";
             silent = true;
             action = "<C-w><${name}>";
             desc = "Go to ${lib.strings.toLower name} pannel.";
