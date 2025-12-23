865980c1a8e66ae4dbd4052711abac0e2ad776c3
Lucas ROUAUD <lucas.rouaud@gmail.com>
2025-12-23 17:36:41 +0100

    Adding rec attributs to the set.

--- core/plugin/local/csvview.nix
+++ core/plugin/local/csvview.nix
@@ -2,14 +2,14 @@
   inherit (pkgs) fetchFromGitHub;
   inherit (pkgs.vimUtils) buildVimPlugin;
 in
-  buildVimPlugin {
+  buildVimPlugin rec {
     pname = "csvview.nvim";
-    version = "v1.3.0";
+    version = "bbab4c2f808fd8e11ec8dfdd128251aadba566a1";
 
     src = fetchFromGitHub {
       owner = "hat0uma";
-      repo = "csvview.nvim";
-      rev = "bbab4c2f808fd8e11ec8dfdd128251aadba566a1";
+      repo = pname;
+      rev = version;
       hash = "sha256-g67Y+OS3BuwEl9NcXzuBnAyPHHRKssxBuF43dIBLpm4=";
     };
   }
